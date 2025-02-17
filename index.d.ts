d0f53e6347523d0f3479cb2304330f157413331b
luolang <657340743@qq.com>
2025-02-17 22:47:29 +0800

    fix types

--- index.d.ts
+++ index.d.ts
@@ -1,20 +1,18 @@
-declare module "node-wordcloud" {
-  const wordcloud: (createCanvas: any) => (
-    canvas: any,
-    options?: {
-      list: [string, number][];
-      gridSize: number;
-      rotateRatio: number;
-      rotationSteps: number;
-      rotationRange: number[];
-      backgroundColor: string;
-      sizeRange: number[];
-      color: string | ((word: string, weight: number) => string);
-      fontFamily: string;
-      fontWeight: string;
-      shape?: string;
-      origin?: number[];
-    }
-  ) => { draw: () => void; updateList: (list: [string, number][]) => void };
-  export default wordcloud;
-}
+const wordcloud: (createCanvas: any) => (
+  canvas: any,
+  options?: {
+    list: [string, number][];
+    gridSize: number;
+    rotateRatio: number;
+    rotationSteps: number;
+    rotationRange: number[];
+    backgroundColor: string;
+    sizeRange: number[];
+    color: string | ((word: string, weight: number) => string);
+    fontFamily: string;
+    fontWeight: string;
+    shape?: string;
+    origin?: number[];
+  }
+) => { draw: () => void; updateList: (list: [string, number][]) => void };
+export default wordcloud;
